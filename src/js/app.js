c909107b37d39941a9469413ab13dae44695218d
lycuong99 <52919981+lycuong99@users.noreply.github.com>
2024-04-06 19:02:17 +0700

    update

--- src/js/app.js
+++ src/js/app.js
@@ -13,6 +13,84 @@ const device = {
   pixelRatio: window.devicePixelRatio
 };
 
+const noise = /*glsl*/`
+//	Classic Perlin 3D Noise 
+//	by Stefan Gustavson
+//
+vec4 permute(vec4 x){return mod(((x*34.0)+1.0)*x, 289.0);}
+vec4 taylorInvSqrt(vec4 r){return 1.79284291400159 - 0.85373472095314 * r;}
+vec3 fade(vec3 t) {return t*t*t*(t*(t*6.0-15.0)+10.0);}
+
+float cnoise(vec3 P){
+  vec3 Pi0 = floor(P); // Integer part for indexing
+  vec3 Pi1 = Pi0 + vec3(1.0); // Integer part + 1
+  Pi0 = mod(Pi0, 289.0);
+  Pi1 = mod(Pi1, 289.0);
+  vec3 Pf0 = fract(P); // Fractional part for interpolation
+  vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
+  vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+  vec4 iy = vec4(Pi0.yy, Pi1.yy);
+  vec4 iz0 = Pi0.zzzz;
+  vec4 iz1 = Pi1.zzzz;
+
+  vec4 ixy = permute(permute(ix) + iy);
+  vec4 ixy0 = permute(ixy + iz0);
+  vec4 ixy1 = permute(ixy + iz1);
+
+  vec4 gx0 = ixy0 / 7.0;
+  vec4 gy0 = fract(floor(gx0) / 7.0) - 0.5;
+  gx0 = fract(gx0);
+  vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
+  vec4 sz0 = step(gz0, vec4(0.0));
+  gx0 -= sz0 * (step(0.0, gx0) - 0.5);
+  gy0 -= sz0 * (step(0.0, gy0) - 0.5);
+
+  vec4 gx1 = ixy1 / 7.0;
+  vec4 gy1 = fract(floor(gx1) / 7.0) - 0.5;
+  gx1 = fract(gx1);
+  vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
+  vec4 sz1 = step(gz1, vec4(0.0));
+  gx1 -= sz1 * (step(0.0, gx1) - 0.5);
+  gy1 -= sz1 * (step(0.0, gy1) - 0.5);
+
+  vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
+  vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
+  vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
+  vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
+  vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
+  vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
+  vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
+  vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
+
+  vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+  g000 *= norm0.x;
+  g010 *= norm0.y;
+  g100 *= norm0.z;
+  g110 *= norm0.w;
+  vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+  g001 *= norm1.x;
+  g011 *= norm1.y;
+  g101 *= norm1.z;
+  g111 *= norm1.w;
+
+  float n000 = dot(g000, Pf0);
+  float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
+  float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
+  float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
+  float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
+  float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
+  float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
+  float n111 = dot(g111, Pf1);
+
+  vec3 fade_xyz = fade(Pf0);
+  vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
+  vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
+  float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x); 
+  return 2.2 * n_xyz;
+}
+`;
+
+
 export default class App {
   constructor(canvas) {
     this.canvas = canvas;
@@ -44,9 +122,9 @@ export default class App {
 
     const spotLight = new THREE.SpotLight(0xffe9e9, 1600);
     spotLight.decay = 1.1;
-    spotLight.angle = Math.PI / 4;
+    spotLight.angle = Math.PI / 2;
     spotLight.distance = 3000;
-    spotLight.position.set(-80, 200, -80);
+    spotLight.position.set(-80 * 3, 200 * 3, -80 * 3);
     spotLight.penumbra = 0.5;
     this.scene.add(spotLight);
 
@@ -128,7 +206,7 @@ export default class App {
           value: textureLoader.load('/texture/fbo.png')
         },
         uState2: {
-          value: textureLoader.load('/texture/state2.jpg')
+          value: textureLoader.load('/texture/state3.png')
         }
       },
       vertexShader: vertex,
@@ -214,6 +292,8 @@ export default class App {
           attribute vec2 instanceUV;
           varying float vHeight;
           varying float vHeightUV;
+
+          ${noise}
           
           `
       );
@@ -222,15 +302,28 @@ export default class App {
         `#include <begin_vertex>`,
         /* glsl */ `
         #include <begin_vertex>
+        
+      
+
+        float n = cnoise(vec3(instanceUV.x*5., instanceUV.y*5., uTime*0.1));  
+        transformed.y += n*60.;
+        //by sin wave cirlce
+        // float dist = distance(instanceUV, vec2( 0.5, 0.5));
+        // transformed.y += (sin((dist )*20.+ uTime* 2.) )*10.;
+      
+
         vHeightUV = clamp(position.y*2., 0.0, 1.0);
         vec4 transition = texture2D(uFBO, instanceUV);
         // (x,y,z) (r,b,g)
         //SCALE
+
         transformed *=  (transition.g);
-        transformed.y =  transition.r*10.;
+        transformed.y +=  transition.r*100.;
 
         vHeight = transformed.y;
-          
+
+
+        
           `
       );
 
@@ -261,7 +354,7 @@ export default class App {
 
           diffuseColor.rgb = ramp_color_two;
           diffuseColor.rgb = mix(diffuseColor.rgb, ramp_color_three, vHeightUV);
-          diffuseColor.rgb = mix(diffuseColor.rgb, hightlight, clamp(vHeight/10., -2., 0.3));
+          diffuseColor.rgb = mix(diffuseColor.rgb, hightlight, clamp(vHeight/10. -3. , 0., 1.));
           `
       );
       // console.log(shader.vertexShader);
@@ -323,6 +416,7 @@ export default class App {
     this.renderer.setRenderTarget(this.fbo);
     this.renderer.render(this.fboScene, this.fboCamera);
 
+    this.uniforms.uTime.value = elapsedTime;
     // dynamic texture -> Instance Mesh Material
     this.uniforms.uFBO.value = this.fbo.texture;
 
--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -13,10 +13,23 @@ void main() {
   float outner_progress = clamp(1.1 * uProgress, 0., 1.);
   float inner_progress = clamp(1.1 * uProgress - 0.05, 0., 1.);
 
-  float innerCircle = 1. - smoothstep((inner_progress - 0.2) * radius, inner_progress * radius, distFromCenter);
+  float deltaCircle = 0.1;
+  // mờ tại 1 khoảng {deltaCircle}
+  // vòng tròn lớn dần theo {inner_progress}
+  // từ đen -> trắng
+  float innerCircle =  1. - smoothstep((inner_progress - deltaCircle) * radius, inner_progress * radius, distFromCenter);
+  
+  float outerCircle = 1. - smoothstep((outner_progress - deltaCircle) * radius, outner_progress * radius, distFromCenter);
+
+  // outerCircle - innerCircle: khoảng giữa 2 circle 
+  float displacement = outerCircle - innerCircle;
+
+  // từ color1 -> color2
+  float scale = mix(color1.r, color2.r, innerCircle);
 
   vec4 finalColor = mix(color1, color2, uProgress);
 
   gl_FragColor = finalColor;
-  gl_FragColor = vec4(vec3(innerCircle), 1.);
+  gl_FragColor = vec4(vec3(displacement , scale, 0.), 1.);
+  
 }
